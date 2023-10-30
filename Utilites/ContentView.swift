06d1b61e1e12394069cdcc32f205f9a99c5f9706
ImGreatest <cmayll@mail.ru>
2023-10-30 19:43:40 +0300

    Initial Commit

---
+++ Utilites/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Utilites
+//
+//  Created by Rayn Sosling on 30.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Utilites/UtilitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilitesApp.swift
+//  Utilites
+//
+//  Created by Rayn Sosling on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct UtilitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
